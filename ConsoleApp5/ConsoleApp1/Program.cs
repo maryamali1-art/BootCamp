c95bb51f223f319a5439743217a4f30bc1f86ebe
maryamali1-art <maryamalihussain02@gmail.com>
2025-07-06 17:23:48 +0300

    string2

--- ConsoleApp5/ConsoleApp1/Program.cs
+++ ConsoleApp5/ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(string1.DetectLanguage());
+            Console.ReadKey();
         }
     }
 }
