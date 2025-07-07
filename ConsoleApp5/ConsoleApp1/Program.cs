37c60db4c9b4decc3bf7ef5c86d1125474cdb8cb
maryamali1-art <maryamalihussain02@gmail.com>
2025-07-07 19:55:56 +0300

    string3

--- ConsoleApp5/ConsoleApp1/Program.cs
+++ ConsoleApp5/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(string1.DetectLanguage());
+            Console.WriteLine(string1.RemoveStopWords());
             Console.ReadKey();
         }
     }
